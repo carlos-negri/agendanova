b6f31bb5b881dd69c51ea10a7b56db404051841d
Alice <alicecoelholied@gmail.com>
2025-04-17 20:03:13 -0300

    paginação e messages

--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from .models import Fornecedor
 from .forms import FornecedorModelForm
 from .models import Fornecedor
 
@@ -16,27 +17,37 @@ class FornecedoresView(ListView):
         qs = super(FornecedoresView, self).get_queryset()
         if buscar:
             return qs.filter(nome__icontains=buscar)
-        return qs
+
+
+        if qs.count()>0:
+            paginator = Paginator(qs, 1)
+            listagem = paginator.get_page(self.request.GET.get('page'))
+            return listagem
+        else:
+            return messages.info(self.request, 'Não existem fornecedores cadastrados!')
 
 
 
 
-class FornecedorAddView(CreateView):
+class FornecedorAddView(SuccessMessageMixin, CreateView):
         model = Fornecedor
         form_class = FornecedorModelForm
         template_name = 'fornecedor_form.html'
         success_url = reverse_lazy('fornecedores')
+        success_message = "Fornecedor cadastrado com sucesso!"
 
 
-
-class FornecedorUpdateView(UpdateView):
+class FornecedorUpdateView(SuccessMessageMixin, UpdateView):
         model = Fornecedor
         form_class = FornecedorModelForm
         template_name = 'fornecedor_form.html'
         success_url = reverse_lazy('fornecedores')
+        success_message = "Fornecedor alterado com sucesso!"
 
 
-class FornecedorDeleteView(DeleteView):
+class FornecedorDeleteView(SuccessMessageMixin, DeleteView):
         model = Fornecedor
         template_name = 'fornecedor_apagar.html'
         success_url = reverse_lazy('fornecedores')
+        success_message = "Fornecedor apagado com sucesso!"
+
