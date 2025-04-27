00ccffd2d7d37aeb292768988711b819ff7f06b4
Alice <alicecoelholied@gmail.com>
2025-04-27 20:40:20 -0300

    funcionarios

--- funcionarios/views.py
+++ funcionarios/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
+from funcionarios.forms import FuncionarioModelForm
 #from .forms import FuncionarioModelForm
 from funcionarios.models import Funcionario
 
@@ -27,3 +28,30 @@ class FuncionariosView(ListView):
             return listagem
         else:
             return messages.info(self.request, 'Não existem funcionários cadastrados!')
+
+
+
+class FuncionarioAddView(SuccessMessageMixin, CreateView):
+    model = Funcionario
+    form_class = FuncionarioModelForm
+    template_name = 'funcionario_form.html'
+    success_url = reverse_lazy('funcionarios')
+    success_message = 'Funcionário cadastrado com sucesso!'
+
+
+
+class FuncionarioUpdateView(SuccessMessageMixin, UpdateView):
+    model = Funcionario
+    form_class = FuncionarioModelForm
+    template_name = 'funcionario_form.html'
+    success_url = reverse_lazy('funcionarios')
+    success_message = 'Funcionário alterado com sucesso!'
+
+
+
+class FuncionarioDeleteView(SuccessMessageMixin, DeleteView):
+    model = Funcionario
+    template_name = 'funcionario_apagar.html'
+    success_url = reverse_lazy('funcionarios')
+    success_message = 'Funcionário apagado com sucesso!'
+
