81a2e833413252d78761c923d5153e73a47a6148
Aluno <Aluno@politecnico.local>
2025-05-05 21:14:51 -0300

    forms inline

--- produtos/models.py
+++ produtos/models.py
@@ -13,3 +13,4 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
+
--- servicos/forms.py
+++ servicos/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.forms import inlineformset_factory
 
-from .models import Servico
+from servicos.models import Servico, ProdutosServico
+
+
 
 class ServicoModelForm(forms.ModelForm):
     class Meta:
@@ -13,3 +16,8 @@ class ServicoModelForm(forms.ModelForm):
             'preco': {'required': 'O preço do serviço é um campo obrigatório.'},
             'descricao': {'required': 'A descrição do serviço é um campo obrigatório.'},
         }
+
+ProdutosServicoInLine = inlineformset_factory(Servico, ProdutosServico, fk_name='servico',
+                                              fields=('produto', 'quantidade'), extra=1, can_delete=True)
+##fk_name especifica qual fk vai ser utilizada
+##nro de formularios que vai mostrar de cada vez
