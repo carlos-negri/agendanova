5b5a5c40cccc79f1fd53b138558860836a5246de
CARLOS <chnnegri@gmail.com>
2025-05-08 21:04:50 -0300

    aula dia 07/05

---
+++ agendamentos/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class Agendamento(models.Model):
+    horario = models.DateTimeField('Horário', help_text='Data e hora do atendimento')
+    cliente = models.ForeignKey('clientes.Cliente', verbose_name='Cliente', help_text='Nome do Cliente',
+                                on_delete=models.PROTECT)
+    funcionario = models.ForeignKey('funcionarios.Funcionario', verbose_name='Funcionário',
+                                    help_text='Nome do Funcionário', on_delete=models.PROTECT)
+    servico = models.ManyToManyField('servicos.Servico', verbose_name='Serviço', through='agendamentos.OrdemServicos')
+
+    @property
+    def servicos(self):
+        return OrdemServicos.objects.filter(agendamento=self)
+
+    class Meta:
+        verbose_name='Agendamento'
+        verbose_name_plural='Agendamentos'
+
+    def __str__(self):
+        return f'Cliente: {self.cliente}'
+
+class OrdemServicos(models.Model):
+    SITUACAO_OPCOES = (
+        ('A', 'Agendado'),
+        ('R', 'Realizado'),
+        ('C', 'Cancelado'),
+    )
+    agendamento = models.ForeignKey('agendamentos.Agendamento', verbose_name='Agendamento',on_delete=models.PROTECT,
+                                    related_name='agendamento')
+    servico = models.ForeignKey('servicos.Servico', verbose_name='Servico', on_delete=models.PROTECT,
+                                related_name='ordem_servico')
+    funcionario = models.ForeignKey('funcionarios.Funcionario', verbose_name='Funcionário',on_delete=models.PROTECT,
+                                    related_name='funcionario')
+    situacao = models.CharField('Situação', max_length=1, choices=SITUACAO_OPCOES, default='A')
+    observacoes = models.TextField('Observações', max_length=300, blank=True, null=True)
+    preco = models.DecimalField('Preço',max_digits=6, decimal_places=2, help_text='Preço do serviço',
+                                default=0.00)
+
+    class Meta:
+        verbose_name = 'Serviço Realizado'
+        verbose_name_plural = 'Serviços Realizados'
+        constraints = [models.UniqueConstraint(fields=['agendamento', 'servico'], name='constraint_agendamento')]
+
+    def __str__(self):
+        return self.servico.nome
